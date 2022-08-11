1f9b84f332e20a41b3a13ea95091a96574ed294b
DaoYuan <txp1035@gmail.com>
2022-08-11 22:59:15 +0800

    feat: utils-node

---
+++ packages/utils-node/.fatherrc.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'father';
+
+export default defineConfig({
+  extends: '../../.fatherrc.base.ts',
+  prebundle: {
+    deps: [
+      'chalk',
+      'yargs-parser',
+      'fs-extra',
+      'lodash',
+      'cross-spawn',
+      'inquirer',
+      'mustache',
+      'axios',
+      'node-fetch',
+    ],
+  },
+});
