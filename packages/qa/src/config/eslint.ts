81b345e8551dbacc751fac900a11bcb1961d104c
DaoYuan <txp1035@gmail.com>
2022-08-11 23:59:10 +0800

    feat: config

---
+++ packages/qa/src/config/eslint.ts
@@ -0,0 +1,121 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import tsEslintConfig from './tsEslintConfig';
+
+const isTypeAwareEnabled = process.env.DISABLE_TYPE_AWARE === undefined;
+
+const parserOptions = {
+  ecmaFeatures: {
+    jsx: true,
+  },
+  babelOptions: {
+    presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'],
+    plugins: [
+      ['@babel/plugin-proposal-decorators', { legacy: true }],
+      ['@babel/plugin-proposal-class-properties', { loose: true }],
+    ],
+  },
+  requireConfigFile: false,
+  project: isTypeAwareEnabled ? './tsconfig.json' : undefined,
+};
+
+const isJsMoreTs = async (paths = 'src') => {
+  const fg = require('fast-glob');
+  const jsFiles = await fg(`${paths}/src/**/*.{js,jsx}`, { deep: 3 });
+  const tsFiles = await fg(`${paths}/src/**/*.{ts,tsx}`, { deep: 3 });
+  return jsFiles.length > tsFiles.length;
+};
+
+const isTsProject = fs.existsSync(path.join(process.cwd() || '.', './tsconfig.json'));
+
+if (isTsProject) {
+  try {
+    isJsMoreTs(process.cwd()).then((jsMoreTs) => {
+      if (!jsMoreTs) return;
+      console.log('这是一个 TypeScript 项目，如果不是请删除 tsconfig.json');
+    });
+  } catch (e) {
+    console.log(e);
+  }
+}
+export default {
+  extends: ['prettier', 'plugin:react/recommended'],
+  parser: '@babel/eslint-parser',
+  plugins: ['react', 'jest', 'unicorn', 'react-hooks'],
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+    mocha: true,
+    jest: true,
+    jasmine: true,
+  },
+  rules: {
+    strict: ['error', 'never'],
+    'react/display-name': 0,
+    'react/jsx-props-no-spreading': 0,
+    'react/state-in-constructor': 0,
+    'react/static-property-placement': 0,
+    // Too restrictive: https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/destructuring-assignment.md
+    'react/destructuring-assignment': 'off',
+    'react/jsx-filename-extension': 'off',
+    'react/no-array-index-key': 'warn',
+    'react-hooks/rules-of-hooks': 'error', // Checks rules of Hooks
+    'react-hooks/exhaustive-deps': 'warn', // Checks deps of Hooks
+    'react/require-default-props': 0,
+    'react/jsx-fragments': 0,
+    'react/jsx-wrap-multilines': 0,
+    'react/prop-types': 0,
+    'react/forbid-prop-types': 0,
+    'react/sort-comp': 0,
+    'react/react-in-jsx-scope': 0,
+    'react/jsx-one-expression-per-line': 0,
+    'generator-star-spacing': 0,
+    'function-paren-newline': 0,
+    'sort-imports': 0,
+    'class-methods-use-this': 0,
+    'no-confusing-arrow': 0,
+    'linebreak-style': 0,
+    // Too restrictive, writing ugly code to defend against a very unlikely scenario: https://eslint.org/docs/rules/no-prototype-builtins
+    'no-prototype-builtins': 'off',
+    'unicorn/prevent-abbreviations': 'off',
+    // Conflict with prettier
+    'arrow-body-style': 0,
+    'arrow-parens': 0,
+    'object-curly-newline': 0,
+    'implicit-arrow-linebreak': 0,
+    'operator-linebreak': 0,
+    'no-param-reassign': 2,
+    'space-before-function-paren': 0,
+    'react/self-closing-comp': 1,
+    'react/jsx-key': 1,
+  },
+  settings: {
+    // support import modules from TypeScript files in JavaScript files
+    'import/resolver': {
+      node: {
+        extensions: isTsProject ? ['.js', '.jsx', '.ts', '.tsx', '.d.ts'] : ['.js', '.jsx'],
+      },
+    },
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx', '.d.ts'],
+    },
+    'import/extensions': ['.js', '.mjs', '.jsx', '.ts', '.tsx', '.d.ts'],
+    'import/external-module-folders': ['node_modules', 'node_modules/@types'],
+    polyfills: ['fetch', 'Promise', 'URL', 'object-assign'],
+    react: {
+      version: 'detect',
+    },
+  },
+  overrides: isTsProject
+    ? [
+        {
+          files: ['**/*.{ts,tsx}'],
+          parser: '@typescript-eslint/parser',
+          rules: tsEslintConfig,
+          extends: ['prettier', 'plugin:@typescript-eslint/recommended'],
+        },
+      ]
+    : [],
+  parserOptions,
+};
---
+++ packages/qa/src/config/index.ts
@@ -0,0 +1,11 @@
+import eslint from './eslint';
+import prettier from './prettier';
+import stylelint from './stylelint';
+import { createTestConfig } from './testing';
+
+export default {
+  stylelint,
+  prettier,
+  eslint,
+  createTestConfig,
+};
