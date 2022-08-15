d9ce5cef25a2c2f4b25322fa64e1b521599cbf11
DaoYuan <txp1035@gmail.com>
2022-08-15 16:12:02 +0800

    feat: template

---
+++ packages/qa/src/template/jest.config.ts
@@ -0,0 +1,15 @@
+import { createTestConfig } from '@txpjs/qa';
+import type { TestConfig } from '@txpjs/qa';
+
+export default {
+  ...createTestConfig(),
+  testMatch: ['<rootDir>/packages/*/src/**/*.test.ts'],
+  modulePathIgnorePatterns: ['<rootDir>/packages/.+/compiled', '<rootDir>/packages/.+/fixtures'],
+  transformIgnorePatterns: ['/node_modules/', '/compiled/'],
+  collectCoverageFrom: [
+    '**/src/**/*.{ts,tsx}',
+    '!**/examples/**/*.{js,jsx,ts,tsx}',
+    '!**/compiled/**/*.{js,jsx}',
+    '!**/fixtures/**/*.*',
+  ],
+} as TestConfig.InitialOptions;
